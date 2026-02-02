3372a2529b483e6381a59e7718d6c7d246beaa06
MGLMendes <miguelmendes203@gmail.com>
2026-02-02 19:39:57 -0300

    8.10. Otimizando testes de persistência com @DataJpaTest

--- src/test/java/com/algaworks/algashop/ordering/infrastructure/persistence/repository/OrderPersistenceEntityRepositoryIT.java
+++ src/test/java/com/algaworks/algashop/ordering/infrastructure/persistence/repository/OrderPersistenceEntityRepositoryIT.java
@@ -5,13 +5,14 @@ import com.algaworks.algashop.ordering.infrastructure.persistence.entity.OrderPe
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 
-@SpringBootTest
-@Transactional
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@DataJpaTest
 class OrderPersistenceEntityRepositoryIT {
 
     private final OrderPersistenceEntityRepository orderRepository;
