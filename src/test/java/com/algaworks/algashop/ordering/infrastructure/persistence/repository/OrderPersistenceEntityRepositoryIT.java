03e96e4d514c44c3bf352f3a3e9c3a42916dbd96
MGLMendes <miguelmendes203@gmail.com>
2026-02-02 19:25:04 -0300

    8.9. Testando modelo de persistência com testes de integração

--- src/test/java/com/algaworks/algashop/ordering/infrastructure/persistence/repository/OrderPersistenceEntityRepositoryIT.java
+++ src/test/java/com/algaworks/algashop/ordering/infrastructure/persistence/repository/OrderPersistenceEntityRepositoryIT.java
@@ -1,14 +1,45 @@
 package com.algaworks.algashop.ordering.infrastructure.persistence.repository;
 
+import com.algaworks.algashop.ordering.domain.model.utility.IdGenerator;
+import com.algaworks.algashop.ordering.infrastructure.persistence.entity.OrderPersistenceEntity;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
 
 @SpringBootTest
+@Transactional
 class OrderPersistenceEntityRepositoryIT {
 
-    @Test
-    public void shouldRun() {
+    private final OrderPersistenceEntityRepository orderRepository;
 
+    @Autowired
+    OrderPersistenceEntityRepositoryIT(OrderPersistenceEntityRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    @Test
+    public void shouldPersist() {
+        long orderId = IdGenerator.generateTSID().toLong();
+        OrderPersistenceEntity entity = OrderPersistenceEntity.builder()
+                .id(orderId)
+                .customerId(IdGenerator.generateTimeBasedUUID())
+                .totalAmount(new BigDecimal(1000))
+                .totalItems(2)
+                .status("DRAF")
+                .paymentMethod("CREDIT_CARD")
+                .build();
+        orderRepository.saveAndFlush(entity);
+
+        Assertions.assertThat(orderRepository.existsById(orderId)).isTrue();
+    }
+
+    @Test
+    public void shouldCount() {
+        Assertions.assertThat(orderRepository.count()).isZero();
     }
 
 }
