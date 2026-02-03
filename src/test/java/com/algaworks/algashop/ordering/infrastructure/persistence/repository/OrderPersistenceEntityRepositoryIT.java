1f89a83e2dffb55ff33a79694a88af9d9c8da4c7
MGLMendes <miguelmendes203@gmail.com>
2026-02-03 19:56:28 -0300

    8.15. Implementando propriedades únicas para o modelo de persistência

--- src/test/java/com/algaworks/algashop/ordering/infrastructure/persistence/repository/OrderPersistenceEntityRepositoryIT.java
+++ src/test/java/com/algaworks/algashop/ordering/infrastructure/persistence/repository/OrderPersistenceEntityRepositoryIT.java
@@ -1,19 +1,24 @@
 package com.algaworks.algashop.ordering.infrastructure.persistence.repository;
 
 import com.algaworks.algashop.ordering.databuilder.OrderPersistenceEntityTestDataBuilder;
+import com.algaworks.algashop.ordering.databuilder.OrderTestDataBuilder;
+import com.algaworks.algashop.ordering.domain.model.entity.Order;
 import com.algaworks.algashop.ordering.domain.model.utility.IdGenerator;
+import com.algaworks.algashop.ordering.infrastructure.persistence.config.SpringDataAuditingConfig;
 import com.algaworks.algashop.ordering.infrastructure.persistence.entity.OrderPersistenceEntity;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @DataJpaTest
+@Import(SpringDataAuditingConfig.class)
 class OrderPersistenceEntityRepositoryIT {
 
     private final OrderPersistenceEntityRepository orderRepository;
@@ -35,6 +40,16 @@ class OrderPersistenceEntityRepositoryIT {
     @Test
     public void shouldCount() {
         Assertions.assertThat(orderRepository.count()).isZero();
+    }
+
+    @Test
+    public void shouldSetAuditingValues() {
+        OrderPersistenceEntity entity = OrderPersistenceEntityTestDataBuilder.existingOrder().build();
+        entity = orderRepository.saveAndFlush(entity);
+
+        Assertions.assertThat(entity.getCreatedByUSerId()).isNotNull();
+        Assertions.assertThat(entity.getLastModifiedAt()).isNotNull();
+        Assertions.assertThat(entity.getLastModifiedByUserId()).isNotNull();
     }
 
 }
