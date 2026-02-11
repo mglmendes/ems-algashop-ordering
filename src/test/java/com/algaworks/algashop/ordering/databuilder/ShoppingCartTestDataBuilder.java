2cbfd9c2329bf00c7a2dfbe2045bed53038b7bde
MGLMendes <miguelmendes203@gmail.com>
2026-02-10 23:56:39 -0300

    8.37. Desafio: Implemente persistência para Shopping Cart

--- src/test/java/com/algaworks/algashop/ordering/databuilder/ShoppingCartTestDataBuilder.java
+++ src/test/java/com/algaworks/algashop/ordering/databuilder/ShoppingCartTestDataBuilder.java
@@ -5,9 +5,11 @@ import com.algaworks.algashop.ordering.domain.model.valueobject.Quantity;
 import com.algaworks.algashop.ordering.domain.model.valueobject.id.CustomerId;
 import com.algaworks.algashop.ordering.domain.model.valueobject.id.ShoppingCartId;
 
+import static com.algaworks.algashop.ordering.databuilder.CustomerTestDataBuilder.DEFAULT_CUSTOMER_ID;
+
 public class ShoppingCartTestDataBuilder {
 
-    public CustomerId customerId = new CustomerId();
+    public CustomerId customerId = DEFAULT_CUSTOMER_ID;
     public static final ShoppingCartId DEFAULT_SHOPPING_CART_ID = new ShoppingCartId();
     private boolean withItems = true;
 
