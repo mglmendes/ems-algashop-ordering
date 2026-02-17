a368553c852fc4a5cd33b565eacc8839d8b475f6
MGLMendes <miguelmendes203@gmail.com>
2026-02-17 13:23:53 -0300

    15.4. Implementação simplificada com Entity Manager

--- src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/customer/repository/CustomerPersistenceEntityRepository.java
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/customer/repository/CustomerPersistenceEntityRepository.java
@@ -1,15 +1,12 @@
 package com.algaworks.algashop.ordering.infrastructure.persistence.customer.repository;
 
 import com.algaworks.algashop.ordering.infrastructure.persistence.customer.entity.CustomerPersistenceEntity;
-import com.algaworks.algashop.ordering.infrastructure.persistence.customer.query.CustomerPersistenceEntityQueries;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 import java.util.UUID;
 
-public interface CustomerPersistenceEntityRepository extends
-        JpaRepository<CustomerPersistenceEntity, UUID>,
-        CustomerPersistenceEntityQueries {
+public interface CustomerPersistenceEntityRepository extends JpaRepository<CustomerPersistenceEntity, UUID> {
     Optional<CustomerPersistenceEntity> findByEmail(String email);
     boolean existsByEmailAndIdNot(String email, UUID customerId);
 }
--- src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/customer/service/CustomerQueryServiceImpl.java
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/customer/service/CustomerQueryServiceImpl.java
@@ -3,24 +3,34 @@ package com.algaworks.algashop.ordering.infrastructure.persistence.customer.serv
 import com.algaworks.algashop.ordering.application.model.customer.output.CustomerOutput;
 import com.algaworks.algashop.ordering.application.model.customer.query.CustomerQueryService;
 import com.algaworks.algashop.ordering.domain.model.customer.exception.CustomerNotFoundException;
-import com.algaworks.algashop.ordering.infrastructure.persistence.customer.repository.CustomerPersistenceEntityRepository;
+import com.algaworks.algashop.ordering.domain.model.customer.jpql.CustomerJPQLQueries;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.TypedQuery;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Objects;
 import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class CustomerQueryServiceImpl implements CustomerQueryService {
 
-    private final CustomerPersistenceEntityRepository customerPersistenceRepository;
+    private final EntityManager entityManager;
 
     @Override
     public CustomerOutput findById(UUID customerId) {
         Objects.requireNonNull(customerId);
-        return customerPersistenceRepository.findByIdAsOutput(customerId).orElseThrow(
-                () -> new CustomerNotFoundException(customerId)
-        );
+        try {
+            TypedQuery<CustomerOutput> query = entityManager.createQuery(CustomerJPQLQueries.findByIdAsOutputJPQL,
+                    CustomerOutput.class);
+            query.setParameter("id", customerId);
+            return query.getSingleResult();
+        } catch (NoResultException e) {
+           throw new CustomerNotFoundException(customerId);
+        }
     }
 }
