d514d607710ed078a343df563e4a1142a14de347
MGLMendes <miguelmendes203@gmail.com>
2026-03-07 14:46:17 -0300

    5.18. Desafio: Adicione o WireMock em ShippingCostServiceIT

--- src/test/java/com/algaworks/algashop/ordering/domain/model/order/ShippingCostServiceIT.java
+++ src/test/java/com/algaworks/algashop/ordering/domain/model/order/ShippingCostServiceIT.java
@@ -3,11 +3,16 @@ package com.algaworks.algashop.ordering.domain.model.order;
 import com.algaworks.algashop.ordering.domain.model.order.shipping.OriginAddressService;
 import com.algaworks.algashop.ordering.domain.model.order.shipping.ShippingCostService;
 import com.algaworks.algashop.ordering.domain.model.common.ZipCode;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.extension.responsetemplating.ResponseTemplateTransformer;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 
 
 @SpringBootTest
@@ -19,14 +24,38 @@ class ShippingCostServiceIT {
     @Autowired
     private OriginAddressService originAddressService;
 
+    private WireMockServer wireMockRapidex;
+
+    @BeforeEach
+    public void setup() {
+        initWireMock();
+    }
+
+    @AfterEach
+    public void clean() {
+        wireMockRapidex.stop();
+    }
+
+    private void initWireMock() {
+        wireMockRapidex = new WireMockServer(options()
+                .port(8780)
+                .usingFilesUnderDirectory("src/test/resources/wiremock/rapidex")
+                .extensions(new ResponseTemplateTransformer(true)));
+
+        wireMockRapidex.start();
+    }
+
     @Test
     void shouldCalculate() {
         ZipCode origin = originAddressService.originAddress().zipCode();
         ZipCode destination = new ZipCode("12345");
 
-        var calculate = shippingCostService.calculate(new ShippingCostService.CalculationRequest(origin, destination));
+        var calculate = shippingCostService
+                .calculate(new ShippingCostService.CalculationRequest(origin, destination));
 
         Assertions.assertThat(calculate.cost()).isNotNull();
         Assertions.assertThat(calculate.expectedDate()).isNotNull();
+        Assertions.assertThat(calculate.expectedDate()).isNotNull();
     }
+
 }
