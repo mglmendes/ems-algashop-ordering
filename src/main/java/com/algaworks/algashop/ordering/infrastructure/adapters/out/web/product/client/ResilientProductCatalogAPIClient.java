8ae1c81144d99083401a0ba77d8074c411997228
MGLMendes <miguelmendes203@gmail.com>
2026-06-07 02:49:05 -0300

    4.10.12. Resiliência com Spring Cloud Circuit Breaker

--- src/main/java/com/algaworks/algashop/ordering/infrastructure/adapters/out/web/product/client/ResilientProductCatalogAPIClient.java
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/adapters/out/web/product/client/ResilientProductCatalogAPIClient.java
@@ -3,11 +3,13 @@ package com.algaworks.algashop.ordering.infrastructure.adapters.out.web.product.
 import com.algaworks.algashop.ordering.infrastructure.adapters.out.web.product.response.ProductResponse;
 import com.algaworks.algashop.ordering.infrastructure.config.exceptionhandler.BadGatewayException;
 import com.algaworks.algashop.ordering.infrastructure.config.exceptionhandler.GatewayTimeoutException;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.cloud.client.circuitbreaker.NoFallbackAvailableException;
+import org.springframework.core.retry.RetryException;
 import org.springframework.resilience.annotation.ConcurrencyLimit;
-import org.springframework.resilience.annotation.Retryable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
@@ -19,25 +21,46 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Component
-@RequiredArgsConstructor
 @Slf4j
 public class ResilientProductCatalogAPIClient {
 
     private final ProductCatalogAPIClient productCatalogAPIClient;
+    private final CircuitBreaker circuitBreaker;
+
+    public ResilientProductCatalogAPIClient(CircuitBreakerFactory circuitBreakerFactory,
+                                            ProductCatalogAPIClient productCatalogAPIClient) {
+        this.productCatalogAPIClient = productCatalogAPIClient;
+        this.circuitBreaker = circuitBreakerFactory.create("productCatalogCB");
+    }
 
     @Cacheable(cacheNames = "algashop:product-catalog-api:v1", key = "#productId")
     @ConcurrencyLimit(10)
-    @Retryable(
-            maxRetries = 3,
-            delayString = "3s",
-            multiplier = 2,
-            includes = {GatewayTimeoutException.class, BadGatewayException.InternalServerErrorException.class}
-    )
     public Optional<ProductResponse> getById(UUID productId) {
+        log.info("Trying to product {}", productId);
+        try {
+            return circuitBreaker.run(() -> loadProduct(productId));
+        } catch (NoFallbackAvailableException e) {
+            if (e.getCause() instanceof RetryException re) {
+                if (re.getCause() instanceof GatewayTimeoutException gte) {
+                    throw gte;
+                }
+                if (re.getCause() instanceof BadGatewayException bte) {
+                    throw bte;
+                }
+            }
+            throw e;
+        }
+
+    }
+
+    private Optional<ProductResponse> loadProduct(UUID productId) {
         log.info("Loading product {}", productId);
         try {
             return Optional.ofNullable(productCatalogAPIClient.getById(productId));
-        } catch (HttpClientErrorException.NotFound e) {
+        } catch (HttpClientErrorException e) {
+            if (e.getCause() instanceof HttpClientErrorException.NotFound) {
+                log.error("Error when loading product {}", productId, e);
+            }
             return Optional.empty();
         } catch (RestClientException e) {
             throw translateException(e);
---
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/config/resilience/SpringCircuitBreakerConfig.java
@@ -0,0 +1,31 @@
+package com.algaworks.algashop.ordering.infrastructure.config.resilience;
+
+import com.algaworks.algashop.ordering.infrastructure.config.exceptionhandler.BadGatewayException;
+import com.algaworks.algashop.ordering.infrastructure.config.exceptionhandler.GatewayTimeoutException;
+import org.springframework.cloud.circuitbreaker.retry.FrameworkRetryCircuitBreakerFactory;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.retry.RetryPolicy;
+
+import java.time.Duration;
+
+@Configuration
+public class SpringCircuitBreakerConfig {
+
+    @Bean
+    public Customizer<FrameworkRetryCircuitBreakerFactory> defaultCustomizer() {
+        RetryPolicy retryPolicy = RetryPolicy.builder()
+                .maxRetries(3)
+                .multiplier(2)
+                .delay(Duration.ofSeconds(3))
+                .includes(GatewayTimeoutException.class, BadGatewayException.InternalServerErrorException.class)
+                .build();
+        return factory -> factory.configure(builder -> builder
+                .retryPolicy(retryPolicy)
+                .openTimeout(Duration.ofSeconds(10))
+                .resetTimeout(Duration.ofSeconds(25))
+                .build(), "productCatalogCB"
+        );
+    }
+}
